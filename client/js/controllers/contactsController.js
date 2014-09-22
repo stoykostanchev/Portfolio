f156f18c5fc2976ba17cfd9b8df8f8bde75bfe12
Stoyko Stanchev <stoykostanchev@gmail.com>
2014-09-22 09:30:30 +0000

    Client : adds dummy pages and navigation

---
+++ client/js/controllers/contactsController.js
@@ -0,0 +1,3 @@
+function ContactsController ($scope) {
+
+}
--- client/js/controllers/projectXcontroller.js
+++ client/js/controllers/projectXcontroller.js
@@ -25,12 +25,23 @@ app.factory('PostsService', function () {
 app.config(['$routeProvider', function ($routeProvider) { 
     $routeProvider
     .when("/", {
-        templateUrl : "templates/navigator.html", 
-        controller  : "NavigatorController"
+        templateUrl : "templates/posts.html", 
+        controller  : "PostsController"
     })
     .when("/Posts", {
-        templateUrl : "templates/posts.html",
+        templateUrl : "templates/posts.html", 
         controller  : "PostsController"
+    })
+    .when('/About', {
+        templateUrl : 'templates/about.html'
+    })
+    .when('/Contact', {
+        templateUrl : 'templates/contact.html',
+        controller  : 'ContactsController'
+    })
+    .when('/Projects', {
+        templateUrl : 'templates/projects.html',
+        controller  : 'ProjectsController'
     })
     .otherwise("/404", {
         templateUrl : "index.html"
