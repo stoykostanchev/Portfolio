a7724e7a32adda9612faaaf835d1131aae7e8cd8
Stoyko Stanchev <stoykostanchev@gmail.com>
2014-10-04 14:30:28 +0000

    Server : modifies the Post API, allowing for an optional fields string to be sent and returns obj if just 1 post has been found

--- server.js
+++ server.js
@@ -47,7 +47,8 @@ router.use('/utils',     express.static(__dirname + '/client/utils'));
 var Post = require('./server/models/Post.js');
 router.route('/server/API/REST/post')
     .get(function(req, res) {
-        Post.find(function(err, posts) {
+        //This will use pipelining for the arguments, once a Mongo model kicks in
+        Post.find({ id : req.query.id }, req.query.fields,function(err, posts) {
             if (err) {
                 res.send(err);
             } else {
--- server/models/Post.js
+++ server/models/Post.js
@@ -10,25 +10,47 @@ var lipsumOpts = {
 
 
 module.exports = {
-    find : function(callback) {
+    find : function(params, fields, callback) {
         var posts = [],
-            count = 5,
+            count = params.id ? 1 : 5,
             now   = new Date();
+            
+        fields = fields ? fields : 'all';
         
         (function getWord() {
             lipsumOpts.amount = 1 + Math.random()*4;
+            
+            if ( fields === 'all' || fields.indexOf("content") > -1) {
+                lipsumOpts.what = 'paragraphs';
+            }
             lipsum.getText(function(text) {
-                var postD = new Date();
+                var postD = new Date(),
+                    post  = {
+                        id      : params.id || count+1,
+                        title   : text,
+                        date    : postD.setDate(now.getDate() - count),
+                        content : text
+                    }, p;
                 
                 count--;
-                posts.push({
-                    title : text,
-                    date  : postD.setDate(now.getDate() - count)
-                });
+               
+                if (fields === 'all' || fields.indexOf('content') === -1) {
+                    //Avoid sending 2 req to Lipsum, just take first X words for title
+                    post['title'] = post['title'].split(/\s+/)
+                                                 .slice(0, lipsumOpts.amount)
+                                                 .join(" ");
+                } else if ( fields !== 'all' ) {
+                    for (p in post) {
+                        if (fields.indexOf(p) === -1){               
+                            delete post[p];
+                        }
+                    }
+                }
+                posts.push(post);
                 if(count) {
                     getWord();
                 } else {
-                    callback(null, posts);
+                    callback(null, posts.length > 1 ? posts : posts[0]);
                 }
             }, lipsumOpts);
         }) ();
