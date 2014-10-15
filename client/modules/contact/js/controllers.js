a6b24dfb7b1f450bba34f81742f35be24576d9fb
Stoyko Stanchev <stoykostanchev@gmail.com>
2014-10-15 10:28:59 +0000

    Client : adds text to the contacts page

--- client/modules/contact/js/controllers.js
+++ client/modules/contact/js/controllers.js
@@ -1,6 +1,8 @@
 angular.module('projectX')
     .controller('ContactsCtrl', function($scope, $state, MessageService) {
-        $scope.message = {};
+        $scope.message = {
+            category : 'none'
+        };
 
         $scope.send = function () {
            MessageService.save($scope.message, function() {
