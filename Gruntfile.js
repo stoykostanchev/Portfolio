107a6394776ccc2ef1cc59c5fdacaf6ea1e432b9
Stoyko Stanchev <stoykostanchev@gmail.com>
2014-11-28 12:54:03 +0000

    Workflow : enforces copying of all assets to the dist version

--- Gruntfile.js
+++ Gruntfile.js
@@ -342,8 +342,7 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '.htaccess',
             'bower_components/**/*',
-            'assets/images/{,*/}*.{webp}',
-            'assets/fonts/**/*',
+            'assets/**/*',
             'index.html'
           ]
         }, {
